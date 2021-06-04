477f30912f1b288dbea91a12df3291b9f4d52ed5
jamesburgess44 <jamesburgess44@yahoo.com>
2021-06-04 14:20:37 -0600

    modify spacing

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ function searchByName(){
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
+   
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
         return false;
