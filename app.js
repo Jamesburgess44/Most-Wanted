262197dbaa7f0dbecb7f34771d96c76c670710f2
carter-willey <cjwilley23@gmail.com>
2021-06-04 15:30:31 -0500

    added input variables and or query input

--- app.js
+++ app.js
@@ -2,14 +2,24 @@
 
 function searchByName(){
     // Grabbing the values from our nameForm form and inputs.
-    let firstNameInput = document.forms['nameForm']['fname'].value;
-    let lastNameInput = document.forms['nameForm']['lname'].value;
+    let firstNameInput = document.forms['criteriaForm']['fname'].value;
+    let lastNameInput = document.forms['criteriaForm']['lname'].value;
+    let genderInput = document.forms['criteriaForm']['gender'].value;
+    let dobInput = document.forms['criteriaForm']['dob'].value;
+    let heightInput = document.forms['criteriaForm']['height'].value;
+    let weightInput = document.forms['criteriaForm']['weight'].value;
+    let eyeColorInput = document.forms['criteriaForm']['eyeColor'].value;
+    let occupationInput = document.forms['criteriaForm']['occupation'].value;
+
+
     //*** */ make variables for each form value
     //**add variables to search seperated by OR statements
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput || person.gender === genderInput ||
+            person.dob === dobInput || person.height === heightInput || person.weight == weightInput ||
+            person.eyeColor === eyeColorInput || person.occupation === occupationInput ){
             return true;
         }
         return false;
